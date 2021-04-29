2f86cd6140b6b7a2333cc6169b58f6b4ed3cdcc0
Junix7 <juliop.unix@gmail.com>
2021-04-28 19:50:02 -0500

    :tada: begin a project
    subir imagen con php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+echo "EJERCICIO: SUBIR IMAGEN USANDO PHP";
+?>
+
+<form enctype="multipart/form-data" action="subir.php" method="POST">
+<input name="uploadedfile" type="file" />
+<input type="submit" value="Subir archivo" />
+</form>
+
+
+
+
+ 
---
+++ subir.php
@@ -0,0 +1,15 @@
+<?php  
+echo "AQUI SE VA A PROCESAR LA SUBIDA DE LA IMAGEN";
+
+$target_path = "uploads/";
+$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
+if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+    echo "El archivo ".  basename( $_FILES['uploadedfile']['name']). 
+    " ha sido subido";
+} else{
+    echo "Ha ocurrido un error, trate de nuevo!";
+}
+
+
+?>
+
